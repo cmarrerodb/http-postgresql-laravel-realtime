9bb9d9291383babbbcbcf1f11989217e133ffc93
cmarrerodb <124000374+cmarrerodb@users.noreply.github.com>
2023-10-22 11:50:14 -0400

    Funcionalidad para la sincronización de tablas

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -8,34 +8,19 @@ class DataController extends Controller
 {
     public function datadb(Request $request) {
         $data = $request->all();
-        // if ($data['tabla'] == 'tabla_base_a') {
-        //     $data['origen'] = 'EL ORIGIEN DE LOS DATOS ES tabla_base_a';
-        //     $nuevoRegistro = new Data();
-        //     $nuevoRegistro->procedencia = 'Recibido de la BD a través de la API';
-        //     $nuevoRegistro->tabla = $request->tabla;
-        //     $nuevoRegistro->valor = $request->texto;
-        //     $nuevoRegistro->save();
-        // } else {
-        //     $data['origen'] = 'LA DATA PROVIENE DESDE OTRA FUENTE';
-        // }
-        switch ($request->tabla) {
-            case 'tabla_base_a':
-                $url='http://127.0.0.1:5000';
-                $envio = ['texto' => $request->texto];
-                break;
-            case 'tabla_base_c':
-                $url = 'http://127.0.0.1:5001';
-                $envio = ['texto' => $request->campo1];
-                break;
+        if ($data['tabla'] == 'tabla_base_a') {
+            $data['origen'] = 'EL ORIGIEN DE LOS DATOS ES tabla_base_a';
+            $nuevoRegistro = new Data();
+            $nuevoRegistro->procedencia = 'Recibido de la BD a través de la API';
+            $nuevoRegistro->tabla = $request->tabla;
+            $nuevoRegistro->valor = $request->texto;
+            $nuevoRegistro->save();
+        } else {
+            $data['origen'] = 'LA DATA PROVIENE DESDE OTRA FUENTE';
         }
-        // $dato = ['texto' => $request->texto];
-        // $response = Http::post('http://127.0.0.1:5000', $dato);
-        $response = Http::post($url, $envio);
-        info($response);
         // info($request->all());
-        // $response = Http::post("http://127.0.0.1:5000", [$data->texto]);
-        // return response()->json(200);
-        // return response()->json(['mensaje' => 'Se ha recibido el dato'], 200);
-        return response()->json(['mensaje' => 1], 200);
+        $response = Http::post("http://127.0.0.1:5000", [$data->texto]);
+        return response()->json(200);
+        return response()->json(['mensaje' => 'Se ha recibido el dato'], 200);
     }    
 }
